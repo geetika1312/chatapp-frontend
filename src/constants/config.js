e932c4211625e35a88759d0ec5f6eb13f691ba6f
geetika1312 <121809849+geetika1312@users.noreply.github.com>
2024-07-26 02:48:41 +0530

    commit

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,8 +1,8 @@
-// export const server = import.meta.env.VITE_SERVER;
+export const server = import.meta.env.VITE_SERVER;
 
 
 
-export const server = import.meta.env.VITE_SERVER || "http://localhost:3000";
+// export const server = import.meta.env.VITE_SERVER || "http://localhost:3000";
 
-console.log("Server URL:", server); // Check the console for the correct value
-console.log("Environment Variables:", import.meta.env);
+// console.log("Server URL:", server); // Check the console for the correct value
+// console.log("Environment Variables:", import.meta.env);
